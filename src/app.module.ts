917f003b947ae640e621fce1f0cc1b92ad2e58a1
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-05-07 15:17:27 +0200

    fix(file): add password for connection to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,6 +16,7 @@ import { FileModule } from './file/file.module';
       port: parseInt(process.env.DB_PORT),
       username: process.env.DB_USER,
       database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
     }),
