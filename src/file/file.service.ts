599321343753ae54e4094533f0a4ef7b7fdabc77
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-05-07 09:21:44 +0200

    feat(file): added uploadFile func

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -3,6 +3,16 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { File } from './file.entity';
 import { FileDTO } from './file.dto';
+import * as AWS from 'aws-sdk';
+import { v4 as uuidv4 } from 'uuid';
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+const s3 = new AWS.S3();
 
 @Injectable()
 export class FileService {
@@ -22,5 +32,26 @@ export class FileService {
     };
 
     await this.fileRepo.save(extendedFileMeta);
+  }
+
+  async uploadFile(file: Express.File): Promise<File> {
+    const s3Key = uuidv4();
+    const uploadParams = {
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: s3Key,
+      Body: file.buffer,
+      ContentType: file.mimetype,
+    };
+
+    await s3.upload(uploadParams).promise();
+
+    const newFile = this.fileRepo.create({
+      original_name: file.originalname,
+      mimetype: file.mimetype,
+      size: file.size,
+      s3_key: s3Key,
+    });
+
+    return this.fileRepo.save(newFile);
   }
 }
